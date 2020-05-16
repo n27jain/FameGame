a2b24c3110b560bdc23234f8beeb4eff989f0727
Naman Jain <n27jain@edu.uwaterloo.com>
2020-05-16 15:13:37 -0400

    Fixed lint issues for functions

--- application/functions/index.js
+++ application/functions/index.js
@@ -67,96 +67,104 @@ exports.onUnFollow = functions.firestore
 
 // when a post is created, add post to timeline of each follower (of post owner)
 exports.onCreatePost = functions.firestore
-  .document("/posts/{userId}/posts/{postId}")
-  .onCreate(async (snapshot, context) => {
-    const postCreated = snapshot.data();
-    const userId = context.params.userId;
-    const postId = context.params.postId;
+    .document("/posts/{userId}/posts/{postId}")
+    .onCreate(async (snapshot, context) => {
+        const postCreated = snapshot.data();
+        const userId = context.params.userId;
+        const postId = context.params.postId;
 
-    // 1) Get all the followers of the user who made the post
-    const userFollowersRef = admin
-      .firestore()
-      .collection("followers")
-      .doc(userId)
-      .collection("usersFollowers");
+        // 1) Get all the followers of the user who made the post
+        const userFollowersRef = admin
+            .firestore()
+            .collection("followers")
+            .doc(userId)
+            .collection("usersFollowers");
 
-    const querySnapshot = await userFollowersRef.get();
-    // 2) Add new post to each follower's timeline
-    querySnapshot.forEach(doc => {
-      const followerId = doc.id;
-      admin
-        .firestore()
-        .collection("timeline")
-        .doc(followerId)
-        .collection("timelinePosts")
-        .doc(postId)
-        .set(postCreated);
+        const querySnapshot = await userFollowersRef.get();
+        // 2) Add new post to each follower's timeline
+        querySnapshot.forEach(doc => {
+            const followerId = doc.id;
+            admin
+                .firestore()
+                .collection("timeline")
+                .doc(followerId)
+                .collection("timelinePosts")
+                .doc(postId)
+                .set(postCreated);
+        });
     });
-  });
 
-// exports.onUpdatePost = functions.firestore
-//   .document("/posts/{userId}/posts/{postId}")
-//   .onUpdate(async (change, context) => {
-//     const postUpdated = change.after.data();
-//     const userId = context.params.userId;
-//     const postId = context.params.postId;
+exports.onUpdatePost = functions.firestore
+    .document("/posts/{userId}/posts/{postId}")
+    .onUpdate(async (change, context) => {
+        const postUpdated = change.after.data();
+        const userId = context.params.userId;
+        const postId = context.params.postId;
 
-//     // 1) Get all the followers of the user who made the post
-//     const userFollowersRef = admin
-//       .firestore()
-//       .collection("followers")
-//       .doc(userId)
-//       .collection("usersFollowers");
+        // 1) Get all the followers of the user who made the post
+        const userFollowersRef = admin
+            .firestore()
+            .collection("followers")
+            .doc(userId)
+            .collection("usersFollowers");
 
-//     const querySnapshot = await userFollowersRef.get();
-//     // 2) Update each post in each follower's timeline
-//     querySnapshot.forEach(doc => {
-//       const followerId = doc.id;
+        const querySnapshot = await userFollowersRef.get();
+        // 2) Update each post in each follower's timeline
+        querySnapshot.forEach(doc => {
+            const followerId = doc.id;
 
-//       admin
-//         .firestore()
-//         .collection("timeline")
-//         .doc(followerId)
-//         .collection("timelinePosts")
-//         .doc(postId)
-//         .get()
-//         .then(doc => {
-//           if (doc.exists) {
-//             doc.ref.update(postUpdated);
-//           }
-//         });
-//     });
-//   });
+            admin
+                .firestore()
+                .collection("timeline")
+                .doc(followerId)
+                .collection("timelinePosts")
+                .doc(postId)
+                .get()
+                .then(doc => {
+                    if (doc.exists) {
+                        return doc.ref.update(postUpdated);
+                    }
+                    else {
+                        throw new Error("No docment found when updating the userpost and trying to find it in the timeline");
+                    }
+                })
+                .catch(error => { return error; });
+        });
+    });
 
-// exports.onDeletePost = functions.firestore
-//   .document("/posts/{userId}/posts/{postId}")
-//   .onDelete(async (snapshot, context) => {
-//     const userId = context.params.userId;
-//     const postId = context.params.postId;
+exports.onDeletePost = functions.firestore
+    .document("/posts/{userId}/posts/{postId}")
+    .onDelete(async (snapshot, context) => {
+        const userId = context.params.userId;
+        const postId = context.params.postId;
 
-//     // 1) Get all the followers of the user who made the post
-//     const userFollowersRef = admin
-//       .firestore()
-//       .collection("followers")
-//       .doc(userId)
-//       .collection("usersFollowers");
+        // 1) Get all the followers of the user who made the post
+        const userFollowersRef = admin
+            .firestore()
+            .collection("followers")
+            .doc(userId)
+            .collection("usersFollowers");
 
-//     const querySnapshot = await userFollowersRef.get();
-//     // 2) Delete each post in each follower's timeline
-//     querySnapshot.forEach(doc => {
-//       const followerId = doc.id;
+        const querySnapshot = await userFollowersRef.get();
+        // 2) Delete each post in each follower's timeline
+        querySnapshot.forEach(doc => {
+            const followerId = doc.id;
 
-//       admin
-//         .firestore()
-//         .collection("timeline")
-//         .doc(followerId)
-//         .collection("timelinePosts")
-//         .doc(postId)
-//         .get()
-//         .then(doc => {
-//           if (doc.exists) {
-//             doc.ref.delete();
-//           }
-//         });
-//     });
-//   });
+            admin
+                .firestore()
+                .collection("timeline")
+                .doc(followerId)
+                .collection("timelinePosts")
+                .doc(postId)
+                .get()
+                .then(doc => {
+                    if (doc.exists) {
+                        return doc.ref.delete();
+                    }
+                    else {
+                        throw new Error("No docment found when deleteing the userpost and trying to find it in the timeline")
+                    }
+                })
+                .catch(error => { return error; });
+        });
+    });
