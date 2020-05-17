f5c6e5fbea6057c62a8de6456dc75942b03b1c3d
Naman Jain <n27jain@edu.uwaterloo.com>
2020-05-17 03:58:06 -0400

    this sucks'

---
+++ application/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter_windows_1.17.0-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\GitProjects\FameGame\TroubleShooting\FameGame\application"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter_windows_1.17.0-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
