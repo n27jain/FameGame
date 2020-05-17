6664c995a1235c255c6cb08b321cfb2cf713ab3b
n27jain <n27jain@uwaterloo.ca>
2020-05-17 18:01:12 -0400

    Dependencies updated. Stable build made

--- application/ios/Flutter/flutter_export_environment.sh
+++ application/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter_windows_1.17.0-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\GitProjects\FameGame\FameGame\application"
+export "FLUTTER_APPLICATION_PATH=E:\GitProjects\FameGame\FameGameOfficial\FameGame\application"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
